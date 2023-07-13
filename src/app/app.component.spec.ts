0e8071cf3d8cef496c7e2ef3e18f1e6e204aedfc
Justin Maxwell <jm00724@my.westga.edu>
2023-07-12 22:55:26 -0400

    resolved e2e tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,6 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'cs6261project4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cs6261project4');
+    expect(app.title).toEqual('Welcome to Resistance Calculator!');
   });
+
 });

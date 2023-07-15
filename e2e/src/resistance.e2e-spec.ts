74f6c9dd05281eb72f1cb609e203f89ca3fbf61c
Justin Maxwell <jm00724@my.westga.edu>
2023-07-15 18:26:20 -0400

    e2e tests added for page layout

--- e2e/src/resistance.e2e-spec.ts
+++ e2e/src/resistance.e2e-spec.ts
@@ -4,4 +4,24 @@ describe('Resistance Calculator', () => {
   it('should have header title that is Welcome to Resistance Calculator!', () => {
     expect(element(by.id('header')).getText()).toEqual('Welcome to Resistance Calculator!');
   });
+
+  it('should have a p tag that says select bands', () => {
+    expect(element(by.id('select-bands')).getText()).toEqual('Select bands');
+  });
+
+  it('should have a p tag that says select bands', () => {
+    expect(element(by.id('select-bands')).getText()).toEqual('Select bands');
+  });
+
+  it('should have a column header for digits', () => {
+    expect(element(by.id('digits-header')).getText()).toEqual('Digits:');
+  });
+
+  it('should have a column header for multiplier', () => {
+    expect(element(by.id('multiplier-header')).getText()).toEqual('Multiplier:');
+  });
+
+  it('should have a column header for tolerance', () => {
+    expect(element(by.id('tolerance-header')).getText()).toEqual('Tolerance:');
+  });
 });
